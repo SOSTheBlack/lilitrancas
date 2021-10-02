2d39780fcd210ef3dac3d72950894d463e1c951e
BILD\jean.garcia <jean.garcia@bild.com.br>
2021-10-02 16:19:18 -0300

    fix tests

---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    public function test_missing_all_inputs()
+    {
+//        $response = $this->post(route(''))
+    }
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
